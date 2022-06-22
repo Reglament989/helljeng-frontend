3f5b159f0ec7c5bc5b3c284ff8fd60fac7c6aac3
Reglament989 <dudecom@tuta.io>
2022-06-22 23:09:43 +0300

    Miss

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:hj_lib/hj_lib.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Hj(baseUrl: "http://localhost");
   runApp(const MyApp());
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cryptography_flutter
 import objectbox_flutter_libs
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  CryptographyFlutterPlugin.register(with: registry.registrar(forPlugin: "CryptographyFlutterPlugin"))
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
