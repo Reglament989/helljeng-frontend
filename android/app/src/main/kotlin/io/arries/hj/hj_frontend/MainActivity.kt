35092498059fc86ad552908da72d9f7d75099acf
Reglament989 <dudecom@tuta.io>
2022-06-22 14:50:04 +0300

    Init

---
+++ android/app/src/main/kotlin/io/arries/hj/hj_frontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.arries.hj.hj_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/room/bloc/room_state.dart
@@ -0,0 +1,6 @@
+part of 'room_bloc.dart';
+
+@immutable
+abstract class RoomState {}
+
+class RoomInitial extends RoomState {}
